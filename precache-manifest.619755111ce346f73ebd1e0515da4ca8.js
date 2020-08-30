838e11da45c669e6ee681c2bfd0be3aafb41245e
Amitav Nott <amitav_nott@ryecountryday.org>
2020-08-30 18:01:57 -0400

    Updates

--- precache-manifest.619755111ce346f73ebd1e0515da4ca8.js
+++ precache-manifest.619755111ce346f73ebd1e0515da4ca8.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "825c8530dc4f7845cef87b95dd478a2f",
+    "revision": "3ef585275f86cbfef112187979dafef3",
     "url": "/base-converter/index.html"
   },
   {
-    "revision": "19cae3ad158d62d821b8",
-    "url": "/base-converter/static/css/main.662bc4a6.chunk.css"
+    "revision": "4cfe30a8a28e09549e24",
+    "url": "/base-converter/static/css/main.25047c07.chunk.css"
   },
   {
     "revision": "87a49b0a93219e524608",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/base-converter/static/js/2.af745234.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "19cae3ad158d62d821b8",
-    "url": "/base-converter/static/js/main.21a607db.chunk.js"
+    "revision": "4cfe30a8a28e09549e24",
+    "url": "/base-converter/static/js/main.c61445e0.chunk.js"
   },
   {
     "revision": "b6e3aa209b964d1b3b54",
